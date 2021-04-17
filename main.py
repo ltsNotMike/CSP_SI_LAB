2b8a3485f14d8a1c190e69852f68b675028e871d
Maciej Witkowski <maciekwitkowski322@gmail.com>
2021-04-17 21:57:14 +0200

    This commit is trash

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
 from problem import Problem
 from einstein_problem import EinsteinProblem
-from variable import Variable
 from constraint import *
 def main():
-	problem = EinsteinProblem()
+
+	problem = Problem()
+	problem.add_variable("T", [i for i in range(1,10)])
+	problem.add_variable("O", [i for i in range(10)])
+	problem.add_variable("F", [i for i in range(1,10)])
+	problem.add_variable("W", [i for i in range(10)])
+	problem.add_variable("R", [i for i in range(10)])
+	problem.add_variable("U", [i for i in range(10)])
+	# Granice
+	def sum_function(t, w, o, f, u, r):
+		is_sat = 2 * (t * 100 + w * 10 + o) == f * 1000 + o * 100 + u * 10 + r
+		if is_sat:
+			print(f"TWO = {t}{w}{o}; FOUR = {f}{o}{u}{r}")
+		return is_sat
+
+	problem.add_constraint(FunctionConstraint(sum_function), ['T', 'W', 'O', 'F', 'U', 'R'])
+	problem.add_constraint(AllDifferentConstraint(), ['T', 'W', 'O', 'F', 'U', 'R'])
+
+
 	problem.get_solution()
+
+	# problem = EinsteinProblem()
+	# problem.get_solution()
 
 if __name__ == "__main__":
 	main()
--- problem.py
+++ problem.py
@@ -14,9 +14,8 @@ class Problem:
 			print("Problem: Tried to add the same variable twice")
 		self.variables[name] = Variable(name, domain)
 
-
 	def get_solution(self) -> map:
-		self.backtracking([copy.deepcopy(x) for x in self.variables])
+		self.forward_checking([copy.deepcopy(x) for x in self.variables])
 		# TODO: This assumes that it is always solved
 		print(self.variables)
 		return self.variables
@@ -33,13 +32,19 @@ class Problem:
 				self.backtracking(unassigned[1:])
 			else:
 				variable.value = None
+	
 	def forward_checking(self, unassigned: list) -> None:
-		# TODO: this has nothing working yet
 		if len(unassigned) == 0:
 			return
 		
 		# Select variable
 		variable = self.variables[unassigned[0]]
+
+		# State push
+		for some_var in unassigned:
+			if some_var != variable:
+				self.variables[some_var].push_domain()
+		
 
 		# For each value in domain
 		for potential_value in variable.domain:
@@ -48,13 +53,37 @@ class Problem:
 
 			# Select value for the variable and remove this value from domains of connected nodes
 			variable.value = potential_value
-			self.forward_check(variable.name, variable.value)
+			self.forward_check(variable.name, unassigned)
 
 			if self.is_satisfying_constraints():
-				self.backtracking(unassigned[1:])
+				self.forward_checking(unassigned[1:])
 			else:
 				variable.value = None
 		
+		for some_var in unassigned:
+			if some_var != variable:
+				self.variables[some_var].pop_domain()
+	
+	def forward_check(self, variable_name: str, unassigned: list):
+		# For each constraint in the problem
+		for constraint, variables in self.constraints:
+			# Check if this constraint is connected with this variable
+			# This could be for all constraints
+			# However i think that this condition make it faster
+			if variable_name in variables:
+				# For each variable that is connected with this constraint ...
+				for connected_variable_name in variables:
+					# ... but is not the provided variable and is not yet assigned
+					if connected_variable_name != variable_name and connected_variable_name in unassigned:
+						# For each value in the domain
+						for value in self.variables[connected_variable_name].domain:
+							# Check if the value breaks the constraint
+							self.variables[connected_variable_name].value = value
+							if not constraint.is_satisfied([self.variables[name] for name in variables]):
+								# Remove the value from domain ...
+								self.variables[connected_variable_name].domain.remove(value)
+						self.variables[connected_variable_name].value = None;
+
 	def is_solved(self):
 		for name in self.variables:
 			if self.variables[name].value == None:
